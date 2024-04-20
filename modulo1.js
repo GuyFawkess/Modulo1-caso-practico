b7e798dde7fefea1b337a348c4e6fbbee5eaa8ee
Jason the noob <jason-ollivierre@hotmail.com>
2024-04-20 19:58:10 +0200

    Terminadoooooooooooo, solo falta jugar un poco con la paleta de colores

--- modulo1.js
+++ modulo1.js
@@ -1,17 +1,81 @@
-function telefono (id) {
-    const element = document.getElementById(id),
-      text = element.innerText.split("");
-    element.innerText = "";
+window.sr = ScrollReveal();
 
-    text.forEach((letter) => {
-      const span = document.createElement("span");
+sr.reveal(".nav", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "-100px",
+});
+sr.reveal(".titulo__letras", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "-100px",
+});
+sr.reveal(".quienSoy__titulo", {
+  duration: 3000,
+  origin: "left",
+  distance: "100px",
+});
+sr.reveal(".info", {
+  duration: 4000,
+  origin: "right",
+  distance: "100px",
+});
+sr.reveal(".fotoperfil", {
+  duration: 4000,
+  origin: "top",
+  distance: "-100px",
+});
 
-      span.className = "num";
+sr.reveal(".estudios__programacion", {
+  duration: 2000,
+  origin: "left",
+  distance: "100px",
+});
+sr.reveal(".estudios__gris", {
+  duration: 3000,
+  origin: "top",
+  distance: "100px",
+});
+sr.reveal(".social", {
+  duration: 2000,
+  origin: "right",
+  distance: "-100px",
+});
+sr.reveal(".menu", {
+  duration: 2000,
+  origin: "top",
+  distance: "-100px",
+});
+sr.reveal(".immune", {
+  duration: 2000,
+  origin: "top",
+  distance: "100px",
+});
 
-      span.innerText = letter;
+// CONTACT SECTION
 
-      element.appendChild(span);
-    });
-  };
-
-telefono("number");
+sr.reveal("#banner", {
+  duration: 3000,
+  origin: "left",
+  distance: "100px",
+});
+sr.reveal(".bootstrap", {
+  duration: 3000,
+  origin: "left",
+  distance: "100px",
+});
+sr.reveal(".form", {
+  duration: 3000,
+  origin: "left",
+  distance: "-100px",
+});
+sr.reveal(".aprobado", {
+  duration: 3000,
+  origin: "top",
+  distance: "100px",
+});
+sr.reveal(".volver", {
+  duration: 3000,
+  origin: "top",
+  distance: "-100px",
+});
